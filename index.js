2f04fbf0145c5dd8f5d651f58440dd79417460e4
Naoto Tokoyoda <tkyd89@gmail.com>
2023-04-21 15:14:30 -0700

    small bugs fixed

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const cors = require('cors');
 app.use(cors());
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5174',
+  origin: 'http://127.0.0.1:5173',
 }));
 
 app.use(express.json())
--- models/User.js
+++ models/User.js
@@ -1,37 +1,39 @@
-const mongoose = require('mongoose')
-const { Schema, model } = mongoose
-const bcrypt = require('bcryptjs')
-const {salt} = require("../config")
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+const bcrypt = require('bcryptjs');
+const { salt } = require('../config');
 
-const UserSchema = new Schema({
-    username:{
-        type:String,
-        required: true,
-        unique: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String, 
-        required: true,
-        unique: true,
+    password: {
+      type: String,
+      required: true,
     },
-    isAdmin:{
-        type: Boolean,
-        default: false,
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
-}, {
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-UserSchema.pre("save", async function(next){
-    if(!this.isModified("password")) return next()
-    const hash = await bcrypt.hash(this.password, salt)
-    this.password = hash
-    next()
-})
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const hash = await bcrypt.hash(this.password, salt);
+  this.password = hash;
+  next();
+});
 
-module.exports = model('User', UserSchema)
+module.exports = model('User', UserSchema);
